3d182714e0d13b27969294047089f920f1b16cfc
Raisal Fani <raisalfismail@gmail.com>
2022-03-03 01:48:33 +0700

    add dart_fundamental_3

--- 5_Basic Dart Programming 2/dart_fundamental_2/bin/dart_fundamental_3.dart
+++ 5_Basic Dart Programming 2/dart_fundamental_2/bin/dart_fundamental_3.dart
@@ -1,3 +1,22 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+// void main(List<String> arguments) {
+//   print('Hello world!');
+// }
+
+import 'dart:io';
+
+void main(List<String> args) async {
+  List<int> nilai = [];
+  int? value = 0;
+  do {
+    value = int.tryParse(stdin.readLineSync() ?? '');
+    nilai.add(value ?? 0);
+  } while (value != null);
+  raisal(nilai, 4);
+}
+
+void raisal(List<int> nilai, int pengali) {
+  for (var i = 0; i < nilai.length; i++) {
+    nilai[i] *= pengali;
+    print(nilai[i]);
+  }
 }
