0e0c8544ab7fd5290dbd2f41bc0712ce4ac6f224
Raisal Fani <raisalfismail@gmail.com>
2022-04-08 17:05:58 +0700

    add flutter_praktikum_19

--- 19_Flutter Navigation/flutter_praktikum_19/lib/main.dart
+++ 19_Flutter Navigation/flutter_praktikum_19/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
